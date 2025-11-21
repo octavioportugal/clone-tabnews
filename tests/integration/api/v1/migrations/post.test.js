4e2cb1bf21491778218feadf8284f2f4f5e78675
Octavio Portugal <106697619+octavioportugal@users.noreply.github.com>
2025-11-21 23:58:21 +0000

    check migrations being runned challenge

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database.js";
+
+beforeAll(clearDataBase);
+
+async function clearDataBase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -5,7 +13,11 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
-
   expect(Array.isArray(responseBody)).toBe(true);
+
+  const checkDBForMigrationsTable = await database.query(
+    "SELECT * FROM pgmigrations;",
+  );
+  console.log(checkDBForMigrationsTable.rows);
+  expect(checkDBForMigrationsTable.rows.length).toBeGreaterThan(0);
 });
