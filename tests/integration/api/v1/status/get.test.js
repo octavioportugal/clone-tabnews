f1d152450c8849a7540f18f40722b9b7a40c2dbc
Octavio Portugal <106697619+octavioportugal@users.noreply.github.com>
2025-12-05 15:24:20 +0000

    test: rename all description to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,18 +3,21 @@ import orchestrator from "tests/orchestrator.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
+describe("GET /api/v1/status", () => {
+  describe("Anonymous User", () => {
+    test("Retrieving system current status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
