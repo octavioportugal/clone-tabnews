2d316bad18ffcb9b12c5ae35866e77ff998ba2e6
Octavio Portugal <106697619+octavioportugal@users.noreply.github.com>
2025-12-11 15:50:56 +0000

    feat: add new `/status` page

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,7 +10,6 @@ describe("GET /api/v1/status", () => {
       expect(response.status).toBe(200);
 
       const responseBody = await response.json();
-      expect(responseBody.updated_at).toBeDefined();
 
       const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
       expect(responseBody.updated_at).toEqual(parseUpdatedAt);
